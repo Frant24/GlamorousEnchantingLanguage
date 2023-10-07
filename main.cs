80954231ec827bfadafbb21b1954a028d0730f1b
Frant24 <36748891+Frant24@users.noreply.github.com>
2023-10-07 05:22:41 +0000

    Main Upload

--- main.cs
+++ main.cs
@@ -1,7 +1,62 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    static void Main()
+    {
+        decimal salesD = 0, salesE = 0, salesF = 0;
+
+        while (true)
+        {
+            Console.WriteLine("Enter salesperson initial (D, E, F) or Z to exit:");
+            char initial = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
+            if (initial == 'Z')
+            {
+                break;
+            }
+
+            Console.WriteLine("Enter sale amount:");
+            decimal sale = decimal.Parse(Console.ReadLine());
+
+            switch (initial)
+            {
+                case 'D':
+                    salesD += sale;
+                    break;
+                case 'E':
+                    salesE += sale;
+                    break;
+                case 'F':
+                    salesF += sale;
+                    break;
+                default:
+                    Console.WriteLine("Error, invalid salesperson selected, please try again");
+                    break;
+            }
+        }
+
+        decimal grandTotal = salesD + salesE + salesF;
+        Console.WriteLine($"Grand Total: ${grandTotal:0,0.00}");
+
+        char highestSalesperson = GetHighestSalesperson(salesD, salesE, salesF);
+        Console.WriteLine($"Highest Sale: {highestSalesperson}");
+    }
+
+    static char GetHighestSalesperson(decimal salesD, decimal salesE, decimal salesF)
+    {
+        if (salesD >= salesE && salesD >= salesF)
+        {
+            return 'D';
+        }
+        else if (salesE >= salesD && salesE >= salesF)
+        {
+            return 'E';
+        }
+        else
+        {
+            return 'F';
+        }
+    }
 }
